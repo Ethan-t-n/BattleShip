009d5cc6ccc6899a8ea89b5f86979faa5f0e2714
Candace Eckels <candace.eckels@gmail.com>
2022-05-17 14:27:16 -0600

    Created code for Ship class attributes

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -11,28 +11,12 @@ RSpec.describe Ship do
     expect(@cruiser).to be_instance_of(Ship)
   end
 
-  it 'has a name' do
-    cruiser = Ship.new("Cruiser")
-    sub = Ship.new("Sub")
-    expect(Ship.name).to eq("Cruiser")
-    expect(Ship.name).to eq("Sub")
+  it 'has attributes' do
+
+    expect(@cruiser.name).to eq("Cruiser")
+    expect(@cruiser.length).to eq(3)
+    expect(@cruiser.health).to eq(3)
   end
-
-
-
-  # it 'has a length' do
-  #   cruiser = Ship.new(3)
-  #   sub = Ship.new(2)
-  #   expect(Ship.length).to eq(3)
-  # end
-
-
-
-  # it 'has health' do
-  #   cruiser = Ship.new(3)
-  #   sub = Ship.new(2)
-  #   expect(Ship.health).to eq(3)
-  # end
 
 
 
@@ -41,18 +25,6 @@ RSpec.describe Ship do
 
   #   expect(Ship.sunk?).to be false
   # end
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 end
