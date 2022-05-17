22732a757d952850485f27d80e0cb0a47775ff07
Ethan Nguyen <etnguyen16@gmail.com>
2022-05-17 15:09:18 -0600

    add attributes

---
+++ lib/cell.rb
@@ -0,0 +1,23 @@
+require 'pry'
+
+class Cell
+  attr_reader :coordinate, :ship
+  def initialize(coordinate)
+    @coordinate = coordinate
+    @ship = ship
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
---
+++ spec/cell_spec.rb
@@ -0,0 +1,49 @@
+require './lib/ship'
+require './lib/cell'
+require 'pry'
+
+
+RSpec.describe Cell do
+  before :each do
+    @cell = Cell.new("B4")
+  end
+
+  it 'exists' do
+    expect(@cell).to be_a(Cell)
+  end
+
+
+  it 'has attributes' do
+    expect(@cell.coordinate).to eq("B4")
+    expect(@cell.ship).to eq(nil)
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
