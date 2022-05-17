bd0e3bc5be96450cb2159b96f74bff0cd16c1aa8
Ethan Nguyen <etnguyen16@gmail.com>
2022-05-17 13:24:26 -0600

    add test for ship

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -5,53 +5,70 @@ require './lib/ship'
 RSpec.describe Ship do
   it 'exists' do
     cruiser = Ship.new("Cruiser", 3)
-    sub = Ship.new("Sub", 2)
     expect(ship).to be_instance_of(Ship)
   end
 
 
 
   it 'has a name' do
-    cruiser = Ship.new("Cruiser")
-    sub = Ship.new("Sub")
+    cruiser = Ship.new("Cruiser", 3)
     expect(Ship.name).to eq("Cruiser")
-    expect(Ship.name).to eq("Sub")
   end
 
 
 
   it 'has a length' do
-    cruiser = Ship.new(3)
-    sub = Ship.new(2)
+    cruiser = Ship.new("Cruiser", 3)
     expect(Ship.length).to eq(3)
   end
 
 
 
   it 'has health' do
-    cruiser = Ship.new(3)
-    sub = Ship.new(2)
+    cruiser = Ship.new("Cruiser", 3)
     expect(Ship.health).to eq(3)
   end
 
 
 
   it 'sunk?' do
-    cruiser = Ship.new(3)
+    cruiser = Ship.new("Cruiser", 3)
 
     expect(Ship.sunk?).to be false
   end
 
 
 
+  it 'hit' do
+   cruiser = Ship.new("Cruiser", 3)
+   cruiser.hit
+
+   expect(cruiser.health).to eq(2)
+ end
+
+ it 'will sink' do
+   cruiser = Ship.new("Cruiser", 3)
+   cruiser.hit
 
 
+   expect(cruiser.health).to eq(2)
+   expect(cruiser.sunk?).to be false
 
 
+   cruiser.hit
 
 
+   expect(cruiser.health).to eq(1)
+   expect(cruiser.sunk?).to be false
 
 
+   cruiser.hit
+
+
+   expect(cruiser.health).to eq(0)
+   expect(cruiser.sunk?).to be true
+
+ end
 
 
 
