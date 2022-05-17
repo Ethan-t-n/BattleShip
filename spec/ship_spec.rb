0f2784ee5640a93b4f29e9cc2b522abd6986a6d9
Ethan Nguyen <etnguyen16@gmail.com>
2022-05-16 21:54:39 -0600

    fix quotations

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -5,7 +5,7 @@ require './lib/ship'
 RSpec.describe Ship do
   it 'exists' do
     cruiser = Ship.new("Cruiser", 3)
-
+    sub = Ship.new("Sub", 2)
     expect(ship).to be_instance_of(Ship)
   end
 
@@ -13,25 +13,37 @@ RSpec.describe Ship do
 
   it 'has a name' do
     cruiser = Ship.new("Cruiser")
-
+    sub = Ship.new("Sub")
     expect(Ship.name).to eq("Cruiser")
+    expect(Ship.name).to eq("Sub")
   end
 
 
 
   it 'has a length' do
-    cruiser = Ship.new("3")
-
-    expect(Ship.length).to eq("3")
+    cruiser = Ship.new(3)
+    sub = Ship.new(2)
+    expect(Ship.length).to eq(3)
   end
 
 
 
   it 'has health' do
-    cruiser = Ship.new("3")
-
-    expect(Ship.health).to eq("3")
+    cruiser = Ship.new(3)
+    sub = Ship.new(2)
+    expect(Ship.health).to eq(3)
   end
+
+
+
+  it 'sunk?' do
+    cruiser = Ship.new(3)
+
+    expect(Ship.sunk?).to be false
+  end
+
+
+
 
 
 
