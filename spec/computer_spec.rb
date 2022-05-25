b615bd2d6b4f95910cfd1fa0a1107cfa95053a86
Candace Eckels <candace.eckels@gmail.com>
2022-05-25 10:53:43 -0600

    Edited computer class

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -15,9 +15,18 @@ RSpec.describe Computer do
   it 'exists and has attributes' do
       expect(@computer).to be_a Computer
       expect(@computer.game_board).to be_a Board
+      expect(@computer.ships).to be_a Array
+
   end
 
   it 'can take choose a random cell' do
-    expect(@computer.random_ship_placement).to be_a Cell
+    expect(@computer.random_cell).to be_a String
   end
+
+  it 'once given a ship it can find random valid coordinates' do
+    #Use a binding.pry to verify this assumption
+    @computer.take_turn(@cruiser)
+  end
+
+
 end
